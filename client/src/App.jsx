dcc286be3faabee3cfa0b0d36c06aec731c44522
cozyneurons <shubhamamn0709@gmail.com>
2025-12-30 12:58:15 +0530

    Feat: Add interactive cursor gradient background

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 import AnimatedRoutes from "./components/AnimatedRoutes";
+import CursorBackground from "./components/CursorBackground";
 
 function App() {
   return (
     <Router>
       <ScrollToTop />
+      <CursorBackground />
       <AnimatedRoutes />
     </Router>
   );
